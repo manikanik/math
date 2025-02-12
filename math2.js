16a4b8306b393b02ba9b7b33d6746ac0eaefaa6d
manikanik <isaacnewton7883@gmail.com>
2025-02-12 11:36:01 +0600

    Update math2.js

--- math2.js
+++ math2.js
@@ -11,10 +11,10 @@ const pd = document.getElementById('pd');
 const pe = document.getElementById('pe');
 
 // a
-let av1 = 'a';
-let av2 = 'b';
-let ax1 = 5;
-let ax2 = 6;
+let av1 = 'x';
+let av2 = 'y';
+let ax1 = 7;
+let ax2 = 12;
 let aop = '+'
 let aopO = '';
 
@@ -26,9 +26,16 @@ if(aop === '+'){
 // a
 
 // p
-let v1 = 'x';
-let x1 = 3;
-let op = '+';
+let v1 = 'y';
+let v2 = `\\frac{1}{${v1}}`;
+let v22 = `\\frac{1}{${v1}^2}`;
+let v23 = `\\frac{1}{${v1}^3}`;
+let v24 = `\\frac{1}{${v1}^4}`;
+let v25 = `\\frac{1}{${v1}^5}`;
+let v26 = `\\frac{1}{${v1}^6}`;
+
+let x1 = 5;
+let op = '-';
 let opO = '';
 
 if(op === '+'){
@@ -38,10 +45,15 @@ if(op === '+'){
 }
 // p
 
-let c = '\\textcolor{limegreen}{\\implies}';
-let d = '\\textcolor{blue}{\\therefore}';
-let e = '\\textcolor{#a15be2}{=}';
-
+let c = `\\textcolor{limegreen}{\\implies}`;
+let d = `\\textcolor{blue}{\\therefore}`;
+let e = `\\textcolor{#a15be2}{=}`;
+let tx1 = `\\textcolor{#076e5d}{\\text{@\\;}}`;
+let and = `\\textcolor{#8f5810}{\\text{\\;and\\;}}`;
+let given = `\\textcolor{#8f5810}{\\text{Given:}}`;
+let here = `\\textcolor{#8f5810}{\\text{Here,}}`;
+let again = `\\textcolor{#8f5810}{\\text{Again,}}`;
+let now = `\\textcolor{#8f5810}{Now,}`;
 
 function gga(){
     let xx1 = x1*x1 - 2;
@@ -54,7 +66,8 @@ function gga(){
         xxx = xx2;
     }
 
-    pa.innerHTML = `\\(Given,\\)<br><br>
+    pa.innerHTML = `\\(${tx1}${v1}${op}${v2}${e}${x1};${v1}^2+${v22}${e}? \\)<br>
+                    \\(${given}\\)<br>
                     \\(${v1}${op}\\frac{1}{${v1}}${e} ${x1}\\)<br><br>
                     \\(${c} \\left(${v1}${op}\\frac{1}{${v1}}\\right)^2${e}${x1}^2 \\)<br><br>
                     \\(${c} (${v1})^2+(\\frac{1}{${v1}})^2${op}2.${v1}.\\frac{1}{${v1}}${e}${x1*x1} \\)<br><br>
@@ -74,7 +87,8 @@ function ggb(){
         xxx = xx2;
     }
 
-    pb.innerHTML = `\\(Given, \\)<br><br>
+    pb.innerHTML = `\\(${tx1}${v1}${op}${v2}${e}${x1};${v1}^3${op}${v23}${e}? \\)<br>
+                    \\(${given}\\)<br>
                     \\(${v1}${op}\\frac{1}{${v1}}${e}${x1} \\)<br><br>
                     \\(${c} (${v1}${op}\\frac{1}{${v1}})^3${e}(${x1})^3 \\)<br><br>
                     \\(${c} (${v1})^3${op}(\\frac{1}{${v1}})^3${op}3.${v1}.\\frac{1}{${v1}}(${v1}${op}\\frac{1}{${v1}})
@@ -95,7 +109,8 @@ function ggc(){
         xxx = xx2;
     }
 
-    pc.innerHTML = `\\(Given,\\)<br><br>
+    pc.innerHTML = `\\(${tx1}${v1}${op}${v2}${e}${x1};${v1}^4+${v24}${e}? \\)<br>
+                    \\(${given}\\)<br>
                     \\(${v1}${op}\\frac{1}{${v1}}${e} ${x1}\\)<br><br>
                     \\(${c} (${v1}${op}\\frac{1}{${v1}})^2${e}${x1}^2 \\)<br><br>
                     \\(${c} (${v1})^2+(\\frac{1}{${v1}})^2${op}2.${v1}.\\frac{1}{${v1}} ${e} ${x1*x1} \\)<br><br>
@@ -126,14 +141,15 @@ function ggd(){
         yyy = yy2;
     }
 
-    pd.innerHTML = `Given,<br>
+    pd.innerHTML = `\\(${tx1}${v1}${op}${v2}${e}${x1};${v1}^5${op}${v25}${e}? \\)<br>
+                    \\(${given}\\)<br>
                     \\(${v1}${op}\\frac{1}{${v1}}${e}${x1} \\)<br><br>
                     \\(${c} (${v1}${op}\\frac{1}{${v1}})^2${e}${x1}^2 \\)<br><br>
                     \\(${c} (${v1})^2+(\\frac{1}{${v1}})^2${op}2.${v1}.\\frac{1}{${v1}}${e}${x1*x1} \\)<br><br>
                     \\(${c} ${v1}^2+\\frac{1}{${v1}^2}${op}2${e}${x1*x1} \\)<br><br>
                     \\(${c} ${v1}^2+\\frac{1}{${v1}^2}${e}${x1*x1}${opO}2 \\)<br><br>
                     \\(${c} ${v1}^2+\\frac{1}{${v1}^2}${e}${xxx} \\)<br><br>
-                    Again,<br>
+                    \\(${again} \\)<br>
                     \\(${v1}${op}\\frac{1}{${v1}}${e}${x1} \\)<br><br>
                     \\(${c} (${v1}${op}\\frac{1}{${v1}})^3${e}${x1}^3 \\)<br><br>
                     \\(${c} (${v1})^3${op}(\\frac{1}{${v1}})^3${op}3.${v1}.\\frac{1}{${v1}}(${v1}${op}\\frac{1}{${v1}})${e}
@@ -141,7 +157,7 @@ function ggd(){
                     \\(${c} ${v1}^3${op}\\frac{1}{${v1}^3}${op}3.${x1}${e}${x1*x1*x1} \\)<br><br>
                     \\(${c} ${v1}^3${op}\\frac{1}{${v1}^3}${e}${x1*x1*x1}${opO}${3*x1} \\)<br><br>
                     \\(${c} ${v1}^3${op}\\frac{1}{${v1}^3}${e}${yyy} \\)<br><br>
-                    Now,<br>
+                    \\(${now} \\)<br>
                     \\((${v1}^3${op}\\frac{1}{${v1}^3})(${v1}^2+\\frac{1}{${v1}^2})${e}${yyy}.${xxx} \\)<br><br>
                     \\(${c} ${v1}^5+${v1}^3.\\frac{1}{${v1}^2}${op}\\frac{1}{${v1}^3}.${v1}^2
                     ${op}\\frac{1}{${v1}^5}${e}${yyy*xxx} \\)<br><br>
@@ -162,7 +178,8 @@ function gge(){
         xxx = xx2;
     }
 
-    pe.innerHTML = `\\(Given,\\)<br><br>
+    pe.innerHTML = `\\(${tx1}${v1}${op}${v2}${e}${x1};${v1}^6+${v26}${e}? \\)<br>
+                    \\(${given}\\)<br>
                     \\(${v1}${op}\\frac{1}{${v1}}${e} ${x1}\\)<br><br>
                     \\(${c} (${v1}${op}\\frac{1}{${v1}})^2${e}${x1}^2 \\)<br><br>
                     \\(${c} (${v1})^2+(\\frac{1}{${v1}})^2${op}2.${v1}.\\frac{1}{${v1}} ${e} ${x1*x1} \\)<br><br>
@@ -188,9 +205,11 @@ function ffa(){
         axxx = axx1 + 2*ax2;
     }
 
-    aa.innerHTML = `\\(Given,\\)<br>
-                    \\(${av1}${aop}${av2}${e}${ax1}\\;and\\; ${av1}${av2}${e}${ax2} \\)<br>
-                    \\(Here, \\)<br>
+    aa.innerHTML = `\\(${tx1}${av1}${aop}${av2}${e}${ax1}${and}${av1}${av2}${e}${ax2};
+                       ${av1}^2+${av2}^2${e}? \\)<br>
+                    \\(${given} \\)<br>
+                    \\(${av1}${aop}${av2}${e}${ax1}${and}${av1}${av2}${e}${ax2} \\)<br>
+                    \\(${here} \\)<br>
                     \\(${av1}${aop}${av2}${e}${ax1} \\)<br>
                     \\(${c}(${av1}${aop}${av2})^2${e}(${ax1})^2 \\)<br>
                     \\(${c}(${av1})^2+(${av2})^2${aop}2.${av1}.${av2}${e}${axx1} \\)<br>
@@ -210,9 +229,11 @@ function ffb(){
         axxx = axx1 + 3*ax1*ax2;
     }
 
-    ab.innerHTML = `\\(Given, \\)<br>
-                    \\(${av1}${aop}${av2}${e}${ax1}\\;and\\; ${av1}${av2}${e}${ax2} \\)<br>
-                    \\(Here, \\)<br>
+    ab.innerHTML = `\\(${tx1}${av1}${aop}${av2}${e}${ax1}${and}${av1}${av2}${e}${ax2};
+                       ${av1}^3${op}${av2}^3${e}? \\)<br>
+                    \\(${given} \\)<br>
+                    \\(${av1}${aop}${av2}${e}${ax1}${and}${av1}${av2}${e}${ax2} \\)<br>
+                    \\(${here} \\)<br>
                     \\(${av1}${aop}${av2}${e}${ax1} \\)<br>
                     \\(${c}(${av1}${aop}${av2})^3${e}(${ax1})^3 \\)<br>
                     \\(${c}(${av1})^3${aop}(${av2})^3${aop}3.${av1}.${av2}.(${av1}${aop}${av2})${e}${axx1} \\)<br>
@@ -231,9 +252,11 @@ function ffc(){
         axxx = axx1 + 2*ax2;
     }
 
-    ac.innerHTML = `\\(Given,\\)<br>
-                    \\(${av1}${aop}${av2}${e}${ax1}\\;and\\; ${av1}${av2}${e}${ax2} \\)<br>
-                    \\(Here, \\)<br>
+    ac.innerHTML = `\\(${tx1}${av1}${aop}${av2}${e}${ax1}${and}${av1}${av2}${e}${ax2};
+                       ${av1}^4+${av2}^4${e}? \\)<br>
+                    \\(${given} \\)<br>
+                    \\(${av1}${aop}${av2}${e}${ax1}${and}${av1}${av2}${e}${ax2} \\)<br>
+                    \\(${here} \\)<br>
                     \\(${av1}${aop}${av2}${e}${ax1} \\)<br>
                     \\(${c}(${av1}${aop}${av2})^2${e}(${ax1})^2 \\)<br>
                     \\(${c}(${av1})^2+(${av2})^2${aop}2.${av1}.${av2}${e}${axx1} \\)<br>
@@ -263,23 +286,25 @@ function ffd(){
         ayyy = ayy1 + 3*ax2*ax1;
     }
 
-    ad.innerHTML = `\\(Given,\\)<br>
-                    \\(${av1}${aop}${av2}${e}${ax1}\\;and\\; ${av1}${av2}${e}${ax2} \\)<br>
-                    \\(Here, \\)<br>
+    ad.innerHTML = `\\(${tx1}${av1}${aop}${av2}${e}${ax1}${and}${av1}${av2}${e}${ax2};
+                       ${av1}^5${op}${av2}^5${e}? \\)<br>
+                    \\(${given} \\)<br>
+                    \\(${av1}${aop}${av2}${e}${ax1}${and}${av1}${av2}${e}${ax2} \\)<br>
+                    \\(${here} \\)<br>
                     \\(${av1}${aop}${av2}${e}${ax1} \\)<br>
                     \\(${c}(${av1}${aop}${av2})^2${e}(${ax1})^2 \\)<br>
                     \\(${c}(${av1})^2+(${av2})^2${aop}2.${av1}.${av2}${e}${axx1} \\)<br>
                     \\(${c}${av1}^2+${av2}^2${aop}2.${ax2}${e}${axx1} \\)<br>
                     \\(${c}${av1}^2+${av2}^2${e}${axx1}${aopO}${2*ax2} \\)<br>
                     \\(${c}${av1}^2+${av2}^2${e}${axxx} \\)<br>
-                    \\(Again, \\)<br>
+                    \\(${again} \\)<br>
                     \\(${av1}${aop}${av2}${e}${ax1} \\)<br>
                     \\(${c}(${av1}${aop}${av2})^3${e}(${ax1})^3 \\)<br>
                     \\(${c}(${av1})^3${aop}(${av2})^3${aop}3.${av1}.${av2}.(${av1}${aop}${av2})${e}${ayy1} \\)<br>
                     \\(${c}${av1}^3${aop}${av2}^3${aop}3.${ax2}.${ax1}${e}${ayy1} \\)<br>
                     \\(${c}${av1}^3${aop}${av2}^3${e}${ayy1}${aopO}${3*ax1*ax2} \\)<br>
                     \\(${c}${av1}^3${aop}${av2}^3${e}${ayyy} \\)<br>
-                    \\(Now, \\)<br>
+                    \\(${now} \\)<br>
                     \\((${av1}^3${aop}${av2}^3)(${av1}^2+${av2}^2)${e}${ayyy}.${axxx} \\)<br>
                     \\(${c}${av1}^5+${av1}^3.${av2}^2${op}${av1}^2.${av2}^3${op}${av2}^5${e}${ayyy*axxx} \\)<br>
                     \\(${c}${av1}^5${op}${av2}^5+${av1}^2.${av2}^2(${av1}${aop}${av2})${e}${ayyy*axxx} \\)<br>
@@ -300,9 +325,11 @@ function ffe(){
         axxx = axx1 + 2*ax2;
     }
 
-    ae.innerHTML = `\\(Given,\\)<br>
-                    \\(${av1}${aop}${av2}${e}${ax1}\\;and\\; ${av1}${av2}${e}${ax2} \\)<br>
-                    \\(Here, \\)<br>
+    ae.innerHTML = `\\(${tx1}${av1}${aop}${av2}${e}${ax1}${and}${av1}${av2}${e}${ax2};
+                       ${av1}^6+${av2}^6${e}? \\)<br>
+                    \\(${given} \\)<br>
+                    \\(${av1}${aop}${av2}${e}${ax1}${and}${av1}${av2}${e}${ax2} \\)<br>
+                    \\(${here} \\)<br>
                     \\(${av1}${aop}${av2}${e}${ax1} \\)<br>
                     \\(${c}(${av1}${aop}${av2})^2${e}(${ax1})^2 \\)<br>
                     \\(${c}(${av1})^2+(${av2})^2${aop}2.${av1}.${av2}${e}${axx1} \\)<br>
@@ -339,27 +366,3 @@ ffc();
 ffd();
 
 ffe();
-
-// let power = 6;
-
-// if(power === 2){
-//     gga();
-// }else if(power === 3){
-//     ggb();
-// }else if(power === 4){
-//     ggc();
-// }else if(power === 5){
-//     ggd();
-// }else if(power === 6){
-//     gge();
-// }else{
-//     aa.innerHTML = 'Out of range. Enter valid number.';
-// }
-
-renderMathInElement(document.body, {
-    delimiters: [
-      { left: "\\(", right: "\\)", display: false },
-      { left: "\\[", right: "\\]", display: true }
-    ]
-  });
-  
